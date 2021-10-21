e7a5631b71078250b0f950d92bc3442d596a8c49
Rodney DuPlessis <35479107+rodneydup@users.noreply.github.com>
2021-10-21 11:50:18 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
+
+THREADS=$1
+if [[ "$THREADS" == "" ]]; then
+  echo "
+   NOTE: you can append '-j#', replacing # with your CPU's number of cores +1, to compile faster.
+   
+   Example: './scripts/run.sh -j5' (for a 4-core processor)
+   "
+fi
+
 (
-  # utilizing cmake's parallel build options
-  # recommended: -j <number of processor cores + 1>
-  # This is supported in cmake >= 3.12 use -- -j5 for older versions
-  cmake --build build/release -j 5
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    cmake --build ./build/release --config Release $THREADS
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    cmake --build ./build/release --config Release $THREADS
+  elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+    cmake --build ./build/release --config Release $THREADS
+  fi
 )
 
 result=$?
 if [ ${result} == 0 ]; then
-  ./bin/Alloscope
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    ./bin/Alloscope
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    open bin/Alloscope
+  elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+    echo "RUN WINDOWS TODO"
+    ./bin/Alloscope
+  fi
 fi
